565e6f69f50ad29f9519efbfae9fbbf8d8e16c58
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-25 01:44:48 +0700

    add vertical variant

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,15 +3,18 @@ import { Component } from "solid-js";
 type DividerProps = {
 	extraClass?: string;
 	dark?: boolean;
+	vertical?: boolean;
 };
 
 export const Divider: Component<DividerProps> = (props) => {
 	return (
 		<div
-			class={`border-b ${props.extraClass}`}
+			class={props.extraClass}
 			classList={{
+				"border-b": !props.vertical,
+				"border-r": props.vertical,
 				"border-neutral-800": props.dark,
-				"border-neutral-600": !props.dark,
+				"border-neutral-700": !props.dark,
 			}}
 		/>
 	);
