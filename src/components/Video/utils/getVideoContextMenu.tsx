dd485bdfa466258d6e9822554819c8da9aa15c24
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-25 01:55:42 +0700

    center text

--- src/components/Video/utils/getVideoContextMenu.tsx
+++ src/components/Video/utils/getVideoContextMenu.tsx
@@ -60,9 +60,9 @@ export const getVideoContextMenu = (props: Props) => {
 				</div>
 				<div class="flex-col-center space-y-2">
 					<div class="font-medium text-center">{props.video.title}</div>
-					<div class="text-sm text-neutral-400 space-y-1">
+					<div class="text-sm text-center text-neutral-400 space-y-1">
 						<div>{props.video.channel.name}</div>
-						<div class="flex flex-row space-x-4 justify-center ">
+						<div class="flex flex-row space-x-4 justify-center">
 							<div>{secondsToTime(props.video.duration)}</div>
 							<Show when={props.video.viewCount} keyed>
 								{(c) => <div>{c.toLocaleString("en-US")} views</div>}
