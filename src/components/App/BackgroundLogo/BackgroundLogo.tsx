bf9d029e97882643a1342ef8014735ac41368874
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-25 11:09:59 +0700

    disable touch action

--- src/components/App/BackgroundLogo/BackgroundLogo.tsx
+++ src/components/App/BackgroundLogo/BackgroundLogo.tsx
@@ -2,7 +2,7 @@ import { Component } from "solid-js";
 
 export const BackgroundLogo: Component = () => {
 	return (
-		<div class="absolute -right-[4rem] opacity-[7.5%] -z-[1000]">
+		<div class="absolute -right-[4rem] opacity-[7.5%] -z-[1000] touch-none pointer-events-none">
 			<img src="/img/bg.png" class="h-screen object-cover" />
 		</div>
 	);
