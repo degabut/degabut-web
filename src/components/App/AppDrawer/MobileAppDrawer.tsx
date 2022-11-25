13937f3e4328a4c0f1436f0f2adedb6e04d5433d
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-25 12:09:24 +0700

    hide now playing when on queue page

--- src/components/App/AppDrawer/MobileAppDrawer.tsx
+++ src/components/App/AppDrawer/MobileAppDrawer.tsx
@@ -1,11 +1,16 @@
-import { Component } from "solid-js";
+import { useLocation } from "solid-app-router";
+import { Component, Show } from "solid-js";
 import { MobileLink } from "./MobileLink";
 import { NowPlayingMobile } from "./NowPlayingMobile";
 
 export const MobileAppDrawer: Component = () => {
+	const location = useLocation();
+
 	return (
 		<div class="flex flex-col w-full h-full">
-			<NowPlayingMobile />
+			<Show when={location.pathname !== "/app/queue"}>
+				<NowPlayingMobile />
+			</Show>
 
 			<div class="flex-row-center flex-wrap bg-black h-full">
 				<MobileLink icon="degabutThin" label="Queue" path="/app/queue" />
