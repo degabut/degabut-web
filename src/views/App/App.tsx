bb09b0f7b5d73519e76ee790cbc220af90c08beb
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-29 14:04:01 +0700

    add error boundary

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,11 +1,13 @@
 import { App } from "@components/App";
+import { Container } from "@components/Container";
+import { RouterLink } from "@components/Link";
 import { AppProvider } from "@providers/AppProvider";
 import { ContextMenuProvider } from "@providers/ContextMenuProvider";
 import { QueueProvider } from "@providers/QueueProvider";
 import { RPCProvider } from "@providers/RPCProvider";
 import { requestPermission } from "@utils";
 import { Outlet, useLocation, useNavigate } from "solid-app-router";
-import { Component, onMount } from "solid-js";
+import { Component, ErrorBoundary, onMount } from "solid-js";
 
 export const RootApp: Component = () => {
 	return (
@@ -43,7 +45,24 @@ const ProvidedApp: Component = () => {
 					<App.BackgroundLogo />
 
 					<div class="h-full overflow-y-auto">
-						<Outlet />
+						<ErrorBoundary
+							fallback={(err) => {
+								console.log(err);
+								return (
+									<Container extraClass="pt-32">
+										<div class="text-9xl">:(</div>
+										<div class="flex flex-col text-xl pt-16 space-y-4">
+											<div>Something went wrong, check console for error details.</div>
+											<RouterLink class="underline underline-offset-2" href="/">
+												Go back
+											</RouterLink>
+										</div>
+									</Container>
+								);
+							}}
+						>
+							<Outlet />
+						</ErrorBoundary>
 					</div>
 
 					<div class="md:hidden block w-full z-10">
