1a4094df7293d8327170b2e4622c4a10fd5c3eaa
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-05 08:39:31 +0700

    add resizeable support for touch device

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -21,8 +21,10 @@ export const Drawer: Component<Props> = (props) => {
 	const startDrag = () => {
 		if (!props.resizeable) return;
 		setIsDragging(true);
-		window.addEventListener("mousemove", handleMouseMove);
+		window.addEventListener("mousemove", handleDragMove);
 		window.addEventListener("mouseup", stopDrag);
+		window.addEventListener("touchmove", handleDragMove);
+		window.addEventListener("touchend", stopDrag);
 	};
 
 	const stopDrag = () => {
@@ -32,16 +34,22 @@ export const Drawer: Component<Props> = (props) => {
 	};
 
 	const cleanup = () => {
-		window.removeEventListener("mousemove", handleMouseMove);
+		window.removeEventListener("mousemove", handleDragMove);
 		window.removeEventListener("mouseup", stopDrag);
+		window.removeEventListener("touchmove", handleDragMove);
+		window.removeEventListener("touchend", stopDrag);
 	};
 
-	const handleMouseMove = throttle((ev: MouseEvent) => {
-		setSize(Math.max(props.right ? window.innerWidth - ev.clientX : ev.clientX, 0));
+	const handleDragMove = throttle((ev: MouseEvent | TouchEvent) => {
+		if ("clientX" in ev) {
+			setSize(Math.max(props.right ? window.innerWidth - ev.clientX : ev.clientX, 0));
+		} else if ("touches" in ev) {
+			setSize(Math.max(props.right ? window.innerWidth - ev.touches[0].clientX : ev.touches[0].clientX, 0));
+		}
 	}, 50);
 
 	return (
-		<div class={`h-full absolute md:static top-0 z-20 ${props.extraContainerClass}`}>
+		<div class="h-full absolute md:static top-0 z-20">
 			{props.isOpen && (
 				<div
 					class="block md:hidden fixed w-screen h-screen top-0 left-0 bg-black bg-opacity-50 -z-50"
@@ -51,7 +59,7 @@ export const Drawer: Component<Props> = (props) => {
 
 			<div
 				class="relative flex flex-row h-full min-w-[4.25rem] max-w-[90vw] md:max-w-sm bg-black overflow-y-auto overflow-x-hidden"
-				classList={{ hidden: !props.isOpen }}
+				classList={{ hidden: !props.isOpen, [props.extraContainerClass || ""]: !!props.extraContainerClass }}
 				style={{ width: `${size()}px` }}
 			>
 				<div class="flex flex-col grow w-full">
@@ -61,6 +69,7 @@ export const Drawer: Component<Props> = (props) => {
 				<Show when={props.resizeable}>
 					<div
 						onMouseDown={startDrag}
+						onTouchStart={startDrag}
 						class="absolute top-0 h-full shrink w-2 border-neutral-600 cursor-col-resize select-none"
 						classList={{
 							"border-r-[1px]": isDragging() && !props.right,
