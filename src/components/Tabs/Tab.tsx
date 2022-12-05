275cdf56162d66f91d8ef12849683060bcedd3a1
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-06 00:12:38 +0700

    adjust padding

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -10,7 +10,10 @@ type Props = {
 export const Tab: Component<Props> = (props) => {
 	return (
 		<div class="flex-grow md:flex-grow-0 cursor-pointer" onClick={() => props.onClick(props.item)}>
-			<div class="px-2 md:px-12 py-3 md:py-2" classList={{ "border-b-2 border-b-white": props.isActive }}>
+			<div
+				class="px-2 md:px-8 lg:px-10 xl:px-12 py-3 md:py-2"
+				classList={{ "border-b-2 border-b-white": props.isActive }}
+			>
 				{props.item.label({ isActive: props.isActive })}
 			</div>
 		</div>
