4d4e581634b557a7b3642db0e2642c7ba7e4ef6b
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-18 22:46:25 +0700

    set playedAt value on track start

--- src/providers/QueueProvider/hooks/useQueueEventListener.ts
+++ src/providers/QueueProvider/hooks/useQueueEventListener.ts
@@ -129,6 +129,7 @@ export const useQueueEventListener = ({ setQueue, setFreezeState, fetchQueue, em
 			return history;
 		});
 		lastTrackSeekedPosition = -1;
+		setQueue("nowPlaying", { playedAt: new Date().toISOString() });
 		setFreezeState({ seek: false });
 	};
 
