7a73a0d4165ffc882dfd5779aaf9c74b428c1efe
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-18 22:45:24 +0700

    remove default target value

--- src/components/A/A.tsx
+++ src/components/A/A.tsx
@@ -1,12 +1,7 @@
-import { IS_DESKTOP } from "@constants";
 import { JSX, ParentComponent } from "solid-js";
 
 type Props = JSX.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const A: ParentComponent<Props> = (props) => {
-	return (
-		<a {...props} target={IS_DESKTOP ? "_blank" : undefined}>
-			{props.children}
-		</a>
-	);
+	return <a {...props}>{props.children}</a>;
 };
