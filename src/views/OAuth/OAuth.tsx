6ec358229eaabc998e2506b4f8b633b34e5b59d4
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-18 22:40:24 +0700

    remove desktop handler

--- src/views/OAuth/OAuth.tsx
+++ src/views/OAuth/OAuth.tsx
@@ -1,47 +1,32 @@
-import { Text } from "@components/Text";
 import { useApi } from "@hooks/useApi";
 import { useLocation, useNavigate } from "@solidjs/router";
-import axios from "axios";
-import { Component, createSignal, onMount } from "solid-js";
+import { Component, onMount } from "solid-js";
 
 export const OAuth: Component = () => {
 	const api = useApi();
-	const [isAuthenticating, setIsAuthenticating] = createSignal(true);
 	const navigate = useNavigate();
 	const location = useLocation();
 
 	onMount(async () => {
 		const code = new URLSearchParams(location.query).get("code");
-		let redirectUri = new URL(import.meta.env.VITE_OAUTH_URL).searchParams.get("redirect_uri");
+		const redirectUri = new URL(import.meta.env.VITE_OAUTH_URL).searchParams.get("redirect_uri");
 		if (!code || !redirectUri) return navigate("/login");
-
-		const isTargetDesktop = location.query["target"] === "desktop";
-		if (isTargetDesktop) redirectUri += "?target=desktop";
 
 		try {
 			const accessToken = await api.auth.getAccessToken(code, redirectUri);
-			if (isTargetDesktop) {
-				await axios.post("http://localhost:39821/oauth", accessToken);
-				setIsAuthenticating(false);
-			} else {
-				api.authManager.setAccessToken(accessToken);
-				const redirect = localStorage.getItem("redirect");
+			api.authManager.setAccessToken(accessToken);
+			const redirect = localStorage.getItem("redirect");
 
-				if (redirect) {
-					localStorage.removeItem("redirect");
-					navigate(redirect);
-				} else {
-					navigate("/app/queue");
-				}
+			if (redirect) {
+				localStorage.removeItem("redirect");
+				navigate(redirect);
+			} else {
+				navigate("/app/queue");
 			}
 		} catch (err) {
 			navigate("/login");
 		}
 	});
 
-	return (
-		<div class="flex-row-center justify-center h-full">
-			<Text.H2>{isAuthenticating() ? "Loading..." : "You can close this tab now :)"}</Text.H2>
-		</div>
-	);
+	return <></>;
 };
