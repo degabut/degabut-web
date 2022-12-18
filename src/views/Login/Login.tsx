f5f562235254da1d0eee0863b7abb150629d55c9
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-18 22:38:44 +0700

    add missing background color

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -15,7 +15,7 @@ export const Login: Component = () => {
 	});
 
 	return (
-		<div class="flex items-center justify-center h-full">
+		<div class="flex items-center justify-center h-full bg-gradient-to-b from-neutral-800 to-neutral-900">
 			<div class="flex-col-center sm:bg-neutral-900 bg-transparent sm:px-16 sm:py-24 rounded-2xl">
 				<img src="/android-chrome-512x512.png" class="w-32 h-32" />
 				<div class="text-4xl font-brand mt-6 font-semibold">degabut</div>
