a90bf4b2e79f1dc595646fb4e4af8ed837a6d490
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-18 22:47:35 +0700

    refactor rpc provider

--- src/providers/RPCProvider/RPCProvider.tsx
+++ src/providers/RPCProvider/RPCProvider.tsx
@@ -1,7 +1,6 @@
 import { IS_BROWSER } from "@constants";
 import { useQueue } from "@hooks/useQueue";
 import { useSettings } from "@hooks/useSettings";
-import { QueueResource } from "@providers/QueueProvider";
 import { createContext, createEffect, onMount, ParentComponent } from "solid-js";
 
 export const RPCContext = createContext();
@@ -12,59 +11,63 @@ export const RPCProvider: ParentComponent = (props) => {
 
 	const { settings } = useSettings();
 	const queue = useQueue();
+	let currentActivity: any = null;
 
-	onMount(() => {
-		updateListeningActivity(queue.data);
-		queue.emitter.on("track-audio-started", () => updateListeningActivity(queue.data));
-		queue.emitter.on("member-added", () => updateListeningActivity(queue.data));
-		queue.emitter.on("member-removed", () => updateListeningActivity(queue.data));
-	});
+	onMount(() => updateListeningActivity());
 
 	createEffect(() => {
-		if (settings.discordRpc) {
-			// TODO handle
-		} else {
-			// TODO handle
-		}
+		if (settings.discordRpc) updateListeningActivity();
+		else window.postMessage({ event: "clear-activity" });
 	});
 
-	const updateListeningActivity = async (queue: Partial<QueueResource>) => {
+	const updateListeningActivity = async () => {
 		if (!settings.discordRpc) return;
 
-		// TODO handle
-		if (!queue.nowPlaying || !queue.voiceChannel) {
-			// {
-			// 	details: "Not listening to anything",
-			// 	state: "💤",
-			// 	assets: {
-			// 		large_image: "degabut",
-			// 		large_text: "Degabut",
-			// 	},
-			// }
+		const nowPlaying = queue.data.nowPlaying;
+		const voiceChannel = queue.data.voiceChannel;
+
+		let data;
+		if (!nowPlaying || !voiceChannel) {
+			data = {
+				details: "Not listening to anything",
+				state: "💤",
+				largeImageText: "Degabut",
+				largeImageKey: "degabut",
+			};
 		} else {
-			const { nowPlaying, voiceChannel } = queue;
 			const title = nowPlaying.video.title;
 			const channelName = nowPlaying.video.channel.name;
-			const otherMemberCount = voiceChannel.members.length - 2;
-			const smallImage = otherMemberCount > 0 ? "multi_user" : "single_user";
-			const smallText =
+			const otherMemberCount = voiceChannel.members.length - 1;
+			const smallImageKey = otherMemberCount > 0 ? "multi_user" : "single_user";
+			const smallImageText =
 				otherMemberCount <= 0
 					? "Listening alone"
 					: `Listening along with ${otherMemberCount} ${otherMemberCount === 1 ? "person" : "people"}`;
-			const start = nowPlaying.playedAt ? Math.floor(new Date(nowPlaying.playedAt).getTime() / 1000) : null;
+			const startTimestamp = nowPlaying.playedAt
+				? Math.floor(new Date(nowPlaying.playedAt).getTime() / 1000)
+				: null;
 
-			// {
-			// 	details: title,
-			// 	state: channelName,
-			// 	assets: {
-			// 		large_image: "degabut",
-			// 		large_text: "Degabut",
-			// 		small_image: smallImage,
-			// 		small_text: smallText,
-			// 	},
-			// 	timestamps: start ? { start } : undefined,
-			// }
+			data = {
+				details: title,
+				state: channelName,
+				largeImageText: "Degabut",
+				largeImageKey: "degabut",
+				smallImageKey,
+				smallImageText,
+				startTimestamp,
+				buttons: [
+					{
+						label: "YouTube",
+						url: "https://youtu.be/" + nowPlaying.video.id,
+					},
+				],
+			};
 		}
+
+		// TODO better way to check current activity
+		if (currentActivity && JSON.stringify(currentActivity) === JSON.stringify(data)) return;
+		currentActivity = data;
+		window.postMessage({ event: "set-activity", data });
 	};
 
 	return <RPCContext.Provider value={{}}>{props.children}</RPCContext.Provider>;
