aa737202b37bb1ecbac9adb6b2036c72a4eb2f3c
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-19 22:59:52 +0700

    default show track on large screen

--- src/views/Zen/Zen.tsx
+++ src/views/Zen/Zen.tsx
@@ -3,15 +3,17 @@ import { Button } from "@components/Button";
 import { Container } from "@components/Container";
 import { Text } from "@components/Text";
 import { useQueue } from "@hooks/useQueue";
+import { useScreen } from "@hooks/useScreen";
 import { useFullscreen } from "@providers/AppProvider";
 import { Component, createSignal, Show } from "solid-js";
 import { PlayerCard, TracksCard } from "./components";
 
 export const Zen: Component = () => {
+	const screen = useScreen();
 	useFullscreen();
 
 	const queue = useQueue();
-	const [isShowTracks, setIsShowTracks] = createSignal(false);
+	const [isShowTracks, setIsShowTracks] = createSignal(screen.gte.md);
 
 	return (
 		<Container
