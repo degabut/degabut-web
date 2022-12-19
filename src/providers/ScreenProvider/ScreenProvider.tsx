3b0bcfdb309c3136a3dcab4370f43d1546de5b05
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-12-19 23:01:24 +0700

    clean up

--- src/providers/ScreenProvider/ScreenProvider.tsx
+++ src/providers/ScreenProvider/ScreenProvider.tsx
@@ -1,11 +1,52 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, ParentComponent } from "solid-js";
-import { defaultScreenValue, Screen, useScreen } from "./hooks";
+import { BreakpointKeys, breakpoints } from "@constants";
+import { throttle } from "@utils";
+import { createContext, onMount, ParentComponent } from "solid-js";
+import { createStore } from "solid-js/store";
+
+type BreakpointEntries = Record<BreakpointKeys, boolean>;
+export type Screen = BreakpointEntries & {
+	lte: BreakpointEntries;
+	gte: BreakpointEntries;
+	size: number;
+	breakpoint: BreakpointKeys;
+};
+
+const breakpointEntries = Object.entries(breakpoints) as [BreakpointKeys, number][];
+const breakpointsKeys = Object.keys(breakpoints) as BreakpointKeys[];
+const defaultBreakpointsEntries = Object.fromEntries(breakpointsKeys.map((k) => [k, false])) as BreakpointEntries;
+export const defaultScreenValue = {
+	...defaultBreakpointsEntries,
+	lte: defaultBreakpointsEntries,
+	gte: defaultBreakpointsEntries,
+	size: 0,
+	breakpoint: "xs" as BreakpointKeys,
+};
+
+const fromEntries = (fn: (k: BreakpointKeys, i: number) => [BreakpointKeys, boolean]) => {
+	return Object.fromEntries(breakpointsKeys.map(fn)) as BreakpointEntries;
+};
 
 export const ScreenContext = createContext<Screen>(defaultScreenValue);
 
 export const ScreenProvider: ParentComponent = (props) => {
-	const screen = useScreen();
+	const [screen, setScreen] = createStore<Screen>(defaultScreenValue);
+
+	const throttledResizeHandler = throttle(() => {
+		const size = window.innerWidth;
+		const breakpoint = (breakpointEntries.find(([, value]) => size >= value)?.[0] || "3xl") as BreakpointKeys;
+		setScreen({
+			size,
+			breakpoint,
+			...fromEntries((k, i) => [k, breakpointsKeys[i] === breakpoint]),
+			lte: fromEntries((k, i) => [k, i < breakpointsKeys.findIndex((b) => b === breakpoint) || k === breakpoint]),
+			gte: fromEntries((k, i) => [k, i > breakpointsKeys.findIndex((b) => b === breakpoint) || k === breakpoint]),
+		});
+	}, 250);
+
+	window.addEventListener("resize", throttledResizeHandler);
+
+	onMount(throttledResizeHandler);
 
 	return <ScreenContext.Provider value={screen}>{props.children}</ScreenContext.Provider>;
 };
