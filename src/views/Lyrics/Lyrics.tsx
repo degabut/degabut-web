c2c2cc5be59306e5d4667a89acb10dbec251e0e8
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-05-02 23:14:04 +0700

    fix auto scrolling transcript

--- src/views/Lyrics/Lyrics.tsx
+++ src/views/Lyrics/Lyrics.tsx
@@ -33,10 +33,10 @@ export const Lyrics: Component = () => {
 	onMount(() => app.setTitle("Lyrics"));
 
 	createEffect(() => {
-		if (transcripts.index() === -1) {
+		if (transcripts.index() === -1 && container) {
 			container.scrollTop = 0;
 		} else {
-			const element = container.childNodes[transcripts.index()] as HTMLDivElement;
+			const element = container?.childNodes[transcripts.index()] as HTMLDivElement;
 			if (!element) return;
 			container.scrollTop = element.offsetTop - container.offsetHeight / 2.5 + element.offsetHeight / 2;
 		}
@@ -44,47 +44,45 @@ export const Lyrics: Component = () => {
 
 	return (
 		<Container size="full" extraClass="h-full" padless>
-			<div class="h-full px-3 md:px-8 py-8 pb-32  overflow-y-auto" ref={container}>
-				<Switch fallback={<LyricsNotFound />}>
-					<Match when={videoTranscripts.isLoading() || lyrics.data.loading}>
-						<Spinner size="lg" />
-					</Match>
-					<Match when={videoTranscripts.data().length}>
-						<div class="space-y-8">
-							<For each={videoTranscripts.data()}>
-								{(t, i) => (
-									<div
-										class="space-y-1"
-										classList={{
-											"text-neutral-300": i() < transcripts.index(),
-											"text-neutral-500": i() > transcripts.index(),
-											"!text-neutral-300": i() === transcripts.index() + 1,
-											"!text-neutral-400": i() === transcripts.index() + 2,
-											"text-xl md:text-2xl": i() !== transcripts.index(),
-											"font-semibold text-2xl md:text-3xl !text-neutral-100":
-												i() === transcripts.index(),
-										}}
-									>
-										<For each={t.texts}>{(text) => <div>{text}</div>}</For>
-									</div>
-								)}
-							</For>
-						</div>
-					</Match>
-					<Match when={lyrics.data()} keyed>
-						{({ content, description }) => (
-							<div class="flex flex-col space-y-8 select-text">
-								<div class="space-y-2">
-									<For each={content.split(/\r?\n/)}>
-										{(t) => <div classList={{ "py-1": !t }}>{t}</div>}
-									</For>
+			<Switch fallback={<LyricsNotFound />}>
+				<Match when={videoTranscripts.isLoading() || lyrics.data.loading}>
+					<Spinner size="lg" />
+				</Match>
+				<Match when={videoTranscripts.data().length}>
+					<div class="space-y-8 h-full overflow-y-auto px-3 md:px-8 py-8 pb-32" ref={container}>
+						<For each={videoTranscripts.data()}>
+							{(t, i) => (
+								<div
+									class="space-y-1"
+									classList={{
+										"text-neutral-300": i() < transcripts.index(),
+										"text-neutral-500": i() > transcripts.index(),
+										"!text-neutral-300": i() === transcripts.index() + 1,
+										"!text-neutral-400": i() === transcripts.index() + 2,
+										"text-xl md:text-2xl": i() !== transcripts.index(),
+										"font-semibold text-2xl md:text-3xl !text-neutral-100":
+											i() === transcripts.index(),
+									}}
+								>
+									<For each={t.texts}>{(text) => <div>{text}</div>}</For>
 								</div>
-								<Text.Body2>{description}</Text.Body2>
+							)}
+						</For>
+					</div>
+				</Match>
+				<Match when={lyrics.data()} keyed>
+					{({ content, description }) => (
+						<div class="flex flex-col space-y-8 select-text px-3 md:px-8 py-8 pb-32">
+							<div class="flex flex-col space-y-2">
+								<For each={content.split(/\r?\n/)}>
+									{(t) => <Text.Body1 classList={{ "py-1": !t }}>{t}</Text.Body1>}
+								</For>
 							</div>
-						)}
-					</Match>
-				</Switch>
-			</div>
+							<Text.Body2>{description}</Text.Body2>
+						</div>
+					)}
+				</Match>
+			</Switch>
 		</Container>
 	);
 };
