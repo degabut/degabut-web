916c6dd5cf8926f5a9c3671dc14eb51f356ee959
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-05-09 15:24:43 +0700

    add empty view

--- src/views/App/components/BottomBar/QueuePlayer.tsx
+++ src/views/App/components/BottomBar/QueuePlayer.tsx
@@ -5,22 +5,42 @@ import { VideoListThumbnail } from "@components/Video";
 import { useQueue } from "@hooks/useQueue";
 import { Component, Show } from "solid-js";
 
+const EmptyQueuePlayer: Component = () => {
+	return (
+		<div class="bg-neutral-950 w-full h-full p-1.5">
+			<RouterLink
+				href="/app/queue/player"
+				class="flex-row-center items-center p-2 z-10 rounded cursor-pointer bg-gray-800"
+			>
+				<div class="flex-row-center space-x-3 truncate">
+					<div class="h-12 w-12 border border-neutral-600 rounded" />
+
+					<Text.Body1 class="text-neutral-400">Nothing is playing</Text.Body1>
+				</div>
+			</RouterLink>
+		</div>
+	);
+};
+
 export const QueuePlayer: Component = () => {
 	const queue = useQueue();
 
 	return (
-		<Show when={queue.data.nowPlaying} keyed>
+		<Show when={queue.data.nowPlaying} keyed fallback={<EmptyQueuePlayer />}>
 			{({ video }) => (
-				<div class="bg-neutral-950 w-full h-full">
+				<div class="bg-neutral-950 w-full h-full relative p-1.5">
 					<Show when={queue.data.position} keyed>
 						{(position) => (
-							<div class="bg-brand-500 h-0.5" style={{ width: `${position / 10 / video.duration}%` }} />
+							<div
+								class="absolute top-0 bg-brand-500 h-0.5 -mx-1.5"
+								style={{ width: `${position / 10 / video.duration}%` }}
+							/>
 						)}
 					</Show>
 
 					<RouterLink
 						href="/app/queue/player"
-						class="relative overflow-hidden flex-row-center m-1.5 p-2 z-10 rounded cursor-pointer bg-gray-800"
+						class="relative overflow-hidden flex-row-center p-2 z-10 rounded cursor-pointer bg-gray-800"
 						title={video.title}
 					>
 						<img
