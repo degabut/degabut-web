cfc904aef3996c701a7685483763398caa47af76
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-10-15 02:11:02 +0700

    add listeners modal

--- src/views/Queue/components/QueueTabs/QueueInfo.tsx
+++ src/views/Queue/components/QueueTabs/QueueInfo.tsx
@@ -1,7 +1,9 @@
-import { Divider, RouterLink, Text } from "@components/atoms";
+import { Button, Divider, Icon, Modal, RouterLink, Text } from "@components/atoms";
+import { Item } from "@components/molecules";
 import { useQueue } from "@hooks/useQueue";
+import { useNavigate } from "@solidjs/router";
 import { secondsToTime } from "@utils/time";
-import { Component, For, JSX } from "solid-js";
+import { Component, For, JSX, createSignal } from "solid-js";
 
 type Props = {
 	title: JSX.Element;
@@ -21,34 +23,75 @@ const InfoItem: Component<Props> = (props) => {
 
 export const QueueInfo: Component = () => {
 	const queue = useQueue();
+	const navigate = useNavigate();
+
+	const [isListenersModalOpen, setIsListenersModalOpen] = createSignal(false);
 
 	const queueDuration = () => {
 		return secondsToTime(queue.data.tracks?.reduce((curr, { video }) => curr + video.duration, 0) || 0);
 	};
 
 	return (
-		<div class="flex flex-row space-x-3 md:space-x-4 py-4 truncate overflow-x-auto">
-			<InfoItem title="Queue Duration" description={queueDuration()}></InfoItem>
+		<>
+			<div class="flex flex-row space-x-3 md:space-x-4 py-4 truncate overflow-x-auto">
+				<InfoItem title="Queue Duration" description={queueDuration()}></InfoItem>
 
-			<Divider vertical />
+				<Divider vertical />
 
-			<InfoItem title="Track Count" description={queue.data.tracks?.length || 0}></InfoItem>
+				<InfoItem title="Track Count" description={queue.data.tracks?.length || 0}></InfoItem>
 
-			<Divider vertical />
+				<Divider vertical />
 
-			<div class="flex-row-center -space-x-2">
-				<For each={queue.data.voiceChannel?.members || []}>
-					{(member) => (
-						<RouterLink href={`/app/recommendation/${member.id}`}>
-							<img
-								title={member.displayName}
-								class="rounded-full w-8 h-8 hover:z-50 relative border-neutral-900 border-2"
-								src={member.avatar || "/img/avatar.png"}
-							/>
-						</RouterLink>
-					)}
-				</For>
+				<div class="flex-row-center space-x-1.5">
+					<Button flat class="w-8 h-8" onClick={() => setIsListenersModalOpen(true)}>
+						<Icon name="people" size="lg" class="fill-neutral-500 mx-auto" />
+					</Button>
+
+					<div class="flex-row-center overflow-x-clip -space-x-2">
+						<For each={queue.data.voiceChannel?.members || []}>
+							{(member) => (
+								<RouterLink href={`/app/recommendation/${member.id}`}>
+									<div class="rounded-full w-8 h-8 hover:z-50 border-neutral-900 border-2">
+										<img
+											title={member.displayName}
+											class="rounded-full"
+											src={member.avatar || "/img/avatar.png"}
+										/>
+									</div>
+								</RouterLink>
+							)}
+						</For>
+					</div>
+				</div>
 			</div>
-		</div>
+
+			<Modal
+				isOpen={isListenersModalOpen()}
+				closeOnEscape
+				onClickOutside={() => setIsListenersModalOpen(false)}
+				extraContainerClass="bg-neutral-900 w-[32rem] top-[15vh] h-[90vh] md:h-[70vh]"
+			>
+				<div class="flex flex-col h-full">
+					<div class="bg-neutral-900 pt-4 md:pt-8 px-2 md:px-8">
+						<Text.H2 class="text-center mb-4">Listeners</Text.H2>
+						<Divider extraClass="my-4" />
+					</div>
+
+					<div class="flex flex-col py-8 px-4 md:p-8 !pt-0 space-y-2 overflow-auto">
+						<For each={queue.data.voiceChannel?.members || []}>
+							{(member) => (
+								<Item.List
+									onClick={() => navigate(`/app/recommendation/${member.id}`)}
+									title={member.displayName}
+									extra={<Text.Caption1>{member.username}</Text.Caption1>}
+									icon={member.avatar || "/img/avatar.png"}
+									extraThumbnailClass="rounded-full"
+								/>
+							)}
+						</For>
+					</div>
+				</div>
+			</Modal>
+		</>
 	);
 };
