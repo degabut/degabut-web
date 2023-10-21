808adb026fbd4069efa927e167eef2b89f603bbd
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-10-21 21:04:27 +0700

    update styling

--- src/components/molecules/Playlist/PlaylistListSkeleton.tsx
+++ src/components/molecules/Playlist/PlaylistListSkeleton.tsx
@@ -3,13 +3,11 @@ import { Component } from "solid-js";
 
 export const PlaylistListSkeleton: Component = () => {
 	return (
-		<div class="flex-row-center space-x-1.5 md:space-x-3 w-full h-[3.625rem] md:pr-2 bg-white/5 animate-pulse rounded cursor-pointer">
+		<div class="flex-row-center space-x-1.5 md:space-x-3 w-full p-1.5">
 			<Skeleton.Image extraClass="h-12 w-12" />
-			<div class="flex flex-col grow shrink space-y-0.5 py-1.5 px-3 truncate">
-				<div class="flex flex-col grow shrink space-y-2 py-1">
-					<Skeleton.Text extraClass="max-w-[32rem] h-4" />
-					<Skeleton.Text extraClass="max-w-[8rem] h-4" />
-				</div>
+			<div class="flex flex-col grow shrink space-y-2 py-1">
+				<Skeleton.Text extraClass="max-w-[32rem] h-4" />
+				<Skeleton.Text extraClass="max-w-[8rem] h-3" />
 			</div>
 		</div>
 	);
