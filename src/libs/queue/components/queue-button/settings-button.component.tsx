dcd9274684909b697f82db5cbe847b537fb3061e
Wilson <58095218+wils446@users.noreply.github.com>
2023-11-15 15:40:03 +0700

    change icon from gear to menu

--- src/libs/queue/components/queue-button/settings-button.component.tsx
+++ src/libs/queue/components/queue-button/settings-button.component.tsx
@@ -44,7 +44,7 @@ export const SettingsButton: Component<Props> = (props) => {
 
 	return (
 		<div
-			use:contextMenu={{
+			use: contextMenu={{
 				items: contextMenuItems(),
 				header: (
 					<div class="flex-col-center justify-center pt-4 pb-8 space-y-1">
@@ -59,7 +59,7 @@ export const SettingsButton: Component<Props> = (props) => {
 			<Button
 				flat
 				class="p-2"
-				icon="gear"
+				icon="menu"
 				iconSize={props.iconSize || "lg"}
 				title="Settings"
 				classList={{
