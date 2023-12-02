c60f8f391d1258b1b9b34a18d8d57ad2df1f51ab
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-12-02 11:37:44 +0700

    fix displaying empty favorite

--- src/apps/recap/views/recap/components/favorite-section.component.tsx
+++ src/apps/recap/views/recap/components/favorite-section.component.tsx
@@ -1,6 +1,6 @@
 import { Text } from "@common/components";
 import { IRecap } from "@user/apis";
-import { Component } from "solid-js";
+import { Component, Show } from "solid-js";
 
 type Props = {
 	recap: IRecap;
@@ -8,7 +8,10 @@ type Props = {
 
 export const FavoriteSection: Component<Props> = (props) => {
 	const favorite = () => {
-		const { video, count } = props.recap.mostPlayed[0];
+		const mostPlayed = props.recap.mostPlayed.at(0);
+		if (!mostPlayed) return null;
+
+		const { video, count } = mostPlayed;
 		const minutes = Math.floor((video.duration * count) / 60);
 		const hours = Math.floor(minutes / 60);
 
@@ -22,19 +25,25 @@ export const FavoriteSection: Component<Props> = (props) => {
 
 	return (
 		<div class="flex-col-center space-y-6">
-			<img src={favorite().video.thumbnails.at(-1)?.url} alt={favorite().video.title} class="w-96" />
-			<div class="text-center">
-				<Text.H2>{favorite().video.title}</Text.H2>
-				<Text.Body2>{favorite().video.channel?.name}</Text.Body2>
-			</div>
-			<div class="flex-col-center text-center">
-				<Text.Body1>
-					You played it <b>{favorite().count}</b> times
-				</Text.Body1>
-				<Text.Body1>
-					That's over <b>{Math.floor(favorite().minutes)}</b> minutes (<b>{favorite().hours}</b> hours)!
-				</Text.Body1>
-			</div>
+			<Show when={favorite()} keyed>
+				{({ video, count, minutes, hours }) => (
+					<>
+						<img src={video.thumbnails.at(-1)?.url} alt={video.title} class="w-96" />
+						<div class="text-center">
+							<Text.H2>{video.title}</Text.H2>
+							<Text.Body2>{video.channel?.name}</Text.Body2>
+						</div>
+						<div class="flex-col-center text-center">
+							<Text.Body1>
+								You played it <b>{count}</b> times
+							</Text.Body1>
+							<Text.Body1>
+								That's over <b>{Math.floor(minutes)}</b> minutes (<b>{hours}</b> hours)!
+							</Text.Body1>
+						</div>
+					</>
+				)}
+			</Show>
 		</div>
 	);
 };
