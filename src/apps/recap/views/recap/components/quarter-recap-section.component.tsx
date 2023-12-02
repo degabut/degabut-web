bb3402eafad4f606bad548531f8146fd5df45a4c
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-12-02 11:02:12 +0700

    use tab

--- src/apps/recap/views/recap/components/quarter-recap-section.component.tsx
+++ src/apps/recap/views/recap/components/quarter-recap-section.component.tsx
@@ -1,7 +1,39 @@
-import { Item, Text } from "@common/components";
+import { Tabs, Text } from "@common/components";
 import { TimeUtil } from "@common/utils";
-import { IRecap } from "@user/apis";
-import { Component, For, Show } from "solid-js";
+import { IMonthly, IRecap } from "@user/apis";
+import { Component, Show } from "solid-js";
+
+const TabContent: Component<{ month: IMonthly }> = (props) => {
+	return (
+		<div class="flex flex-col space-y-8 w-full">
+			<div class="flex justify-evenly text-center">
+				<Text.H2 class="font-normal">
+					<b>{props.month.songPlayed}</b> song played
+				</Text.H2>
+				<Text.H2 class="font-normal">
+					Listened for <b>{Math.floor(props.month.durationPlayed / 60)} mins</b>
+				</Text.H2>
+			</div>
+
+			<Show when={props.month.mostPlayed} keyed>
+				{({ video }) => (
+					<div class="flex-col-center space-y-3">
+						<Text.H2>Top Song</Text.H2>
+						<img
+							src={video.thumbnails.at(0)?.url}
+							alt={video.title}
+							class="w-24 aspect-square object-cover"
+						/>
+						<div class="text-center w-full truncate">
+							<Text.H3 class="truncate">{video.title}</Text.H3>
+							<Text.Body2>{video.channel?.name}</Text.Body2>
+						</div>
+					</div>
+				)}
+			</Show>
+		</div>
+	);
+};
 
 type Props = {
 	quarter: 1 | 2 | 3 | 4;
@@ -15,47 +47,31 @@ export const QuarterRecapSection: Component<Props> = (props) => {
 
 	return (
 		<div class="w-full max-w-xl">
-			<For each={quarterData()}>
-				{({ month, durationPlayed, songPlayed, mostPlayed }, i) => (
-					<>
-						<div class="flex-col-center space-y-4">
-							<Text.H2>{TimeUtil.getMonths(month)}</Text.H2>
-
-							<div class="space-y-2 w-full">
-								<div class="flex flex-row justify-center text-center space-x-6 w-full">
-									<Text.Body1>
-										<b>{songPlayed}</b> song played
-									</Text.Body1>
-									<Text.Body1>
-										Listened for <b>{Math.floor(durationPlayed / 60)} mins</b>
-									</Text.Body1>
-								</div>
-
-								<Show when={mostPlayed} keyed>
-									{({ video, count }) => (
-										<div class="truncate mx-auto">
-											<Item.List
-												extraContainerClass="w-min mx-auto truncate"
-												imageUrl={video.thumbnails.at(0)?.url}
-												title={video.title}
-												extra={() => (
-													<Text.Caption1>
-														Played {count} times - {video.channel?.name}
-													</Text.Caption1>
-												)}
-											/>
-										</div>
-									)}
-								</Show>
-							</div>
-						</div>
-
-						<Show when={i() !== 2}>
-							<div class="border-b border-neutral-700 my-4" />
-						</Show>
-					</>
+			<Show when={quarterData()} keyed>
+				{([first, second, third]) => (
+					<Tabs
+						extraContentContainerClass="pt-8"
+						extraTabClass="flex-1"
+						items={[
+							{
+								id: "1",
+								labelText: TimeUtil.getMonths(first.month),
+								element: () => <TabContent month={first} />,
+							},
+							{
+								id: "2",
+								labelText: TimeUtil.getMonths(second.month),
+								element: () => <TabContent month={second} />,
+							},
+							{
+								id: "3",
+								labelText: TimeUtil.getMonths(third.month),
+								element: () => <TabContent month={third} />,
+							},
+						]}
+					/>
 				)}
-			</For>
+			</Show>
 		</div>
 	);
 };
