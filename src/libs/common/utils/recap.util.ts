1f380d0ee5e67f3e4bef2c37a1de27531eea25e8
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-01-16 10:53:13 +0700

    adjust time

--- src/libs/common/utils/recap.util.ts
+++ src/libs/common/utils/recap.util.ts
@@ -1,8 +1,8 @@
 export class RecapUtil {
 	static getYear() {
 		const date = new Date();
-		if (date.getMonth() === 11) return date.getFullYear();
-		else if (date.getMonth() === 0) return date.getFullYear() - 1;
+		if (date.getMonth() === 11 && date.getDate() >= 15) return date.getFullYear();
+		else if (date.getMonth() === 0 && date.getDate() <= 15) return date.getFullYear() - 1;
 		else return null;
 	}
 }
