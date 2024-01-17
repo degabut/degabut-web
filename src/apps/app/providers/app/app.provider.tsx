7c384e2b16b5c5ccac65f5a96995d4df496b1096
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-01-17 15:05:27 +0700

    add confirmation state

--- src/apps/app/providers/app/app.provider.tsx
+++ src/apps/app/providers/app/app.provider.tsx
@@ -2,22 +2,24 @@
 import { useShortcut } from "@common/hooks";
 import { IMediaSource } from "@media-source/apis";
 import { AddPlaylistMediaSourceModal } from "@playlist/components";
-import { Accessor, JSX, ParentComponent, Setter, createContext, createSignal } from "solid-js";
+import { Accessor, JSX, ParentComponent, Setter, Show, createContext, createSignal } from "solid-js";
+import { SetStoreFunction } from "solid-js/store";
 import { ConfirmationModal, QuickSearchModal } from "./components";
 import { useCatJam, useSnowfall, useVersionCheck, useZoom } from "./hooks";
 
-type Confirmation = {
+type Confirmation<T = object> = {
 	title: string;
-	message?: string | (() => JSX.Element);
+	message?: string | ((state: T, setState: SetStoreFunction<T>) => JSX.Element);
 	isAlert?: boolean;
-	onConfirm?: () => unknown | Promise<unknown>;
+	state?: T;
+	onConfirm?: (state: T) => unknown | Promise<unknown>;
 };
 
 export type AppContextStore = {
 	title: Accessor<string>;
 	setTitle: (title: string) => void;
 	promptAddMediaToPlaylist: (media: IMediaSource | null) => void;
-	setConfirmation: (confirmation: Confirmation | null) => void;
+	setConfirmation: <T>(confirmation: Confirmation<T> | null) => void;
 	setIsQuickSearchModalOpen: Setter<boolean>;
 	hasNewVersion: Accessor<boolean>;
 };
@@ -41,11 +43,6 @@ export const AppProvider: ParentComponent = (props) => {
 	const [mediaPlaylist, setMediaPlaylist] = createSignal<null | IMediaSource>(null);
 	const [isQuickSearchModalOpen, setIsQuickSearchModalOpen] = createSignal(false);
 	const [confirmation, setConfirmation] = createSignal<Confirmation | null>(null);
-
-	const onConfirm = async () => {
-		await confirmation()?.onConfirm?.();
-		setConfirmation(null);
-	};
 
 	useShortcut({
 		shortcuts: [
@@ -75,14 +72,21 @@ export const AppProvider: ParentComponent = (props) => {
 				onClose={() => setMediaPlaylist(null)}
 			/>
 
-			<ConfirmationModal
-				isOpen={!!confirmation()}
-				title={confirmation()?.title || ""}
-				message={confirmation()?.message}
-				isAlert={!!confirmation()?.isAlert}
-				onConfirm={onConfirm}
-				onClose={() => setConfirmation(null)}
-			/>
+			<Show when={confirmation()} keyed>
+				{(c) => (
+					<ConfirmationModal
+						title={c.title || ""}
+						message={c.message}
+						isAlert={!!c.isAlert}
+						onConfirm={(state) => {
+							c.onConfirm?.(state);
+							setConfirmation(null);
+						}}
+						initialState={c.state}
+						onClose={() => setConfirmation(null)}
+					/>
+				)}
+			</Show>
 		</AppContext.Provider>
 	);
 };
--- src/apps/app/providers/app/components/confirmation-modal/confirmation-modal.component.tsx
+++ src/apps/app/providers/app/components/confirmation-modal/confirmation-modal.component.tsx
@@ -1,25 +1,27 @@
 import { Button, Modal } from "@common/components";
 import { Component, JSX, Show, createSignal, onCleanup, onMount } from "solid-js";
+import { SetStoreFunction, createStore } from "solid-js/store";
 
 type Props = {
 	title: string;
-	message?: string | (() => JSX.Element);
+	message?: string | ((state: object, setState: SetStoreFunction<object>) => JSX.Element);
+	initialState?: object;
 	isAlert: boolean;
-	isOpen: boolean;
-	onConfirm: () => void | Promise<void>;
+	onConfirm: (state: object) => void | Promise<void>;
 	onClose: () => void;
 };
 
 export const ConfirmationModal: Component<Props> = (props) => {
 	const [isLoading, setIsLoading] = createSignal(false);
+	const [state, setState] = createStore<object>(props.initialState);
 
 	const onKeyDown = (e: KeyboardEvent) => {
-		if (e.key === "Enter") props.onConfirm();
+		if (e.key === "Enter") props.onConfirm(state);
 	};
 
 	const onConfirm = async () => {
 		setIsLoading(true);
-		await props.onConfirm();
+		await props.onConfirm(state);
 		setIsLoading(false);
 	};
 
@@ -34,13 +36,17 @@ export const ConfirmationModal: Component<Props> = (props) => {
 	return (
 		<Modal
 			extraContainerClass="absolute w-[32rem] max-h-[70vh] break-words"
-			isOpen={props.isOpen}
+			isOpen
 			closeOnEscape
 			onClickOutside={() => props.onClose()}
 		>
 			<div class="space-y-8 p-8">
 				<div class="text-xl font-medium text-center mb-4">{props.title}</div>
-				{typeof props.message === "string" ? <div class="text-center">{props.message}</div> : props.message?.()}
+				{typeof props.message === "string" ? (
+					<div class="text-center">{props.message}</div>
+				) : (
+					props.message?.(state, setState)
+				)}
 
 				<div class="flex-row-center justify-evenly w-full">
 					<Show when={!props.isAlert}>
