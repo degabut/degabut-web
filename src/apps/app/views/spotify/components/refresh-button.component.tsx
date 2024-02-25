db5f94782d865a66dbcc048e22326f684d5a1696
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-02-25 12:30:23 +0700

    update padding

--- src/apps/app/views/spotify/components/refresh-button.component.tsx
+++ src/apps/app/views/spotify/components/refresh-button.component.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const RefreshButton: Component<Props> = (props) => {
 	return (
-		<Button flat class="px-1 py-0.5" onClick={props.onClick} disabled={props.disabled}>
+		<Button flat class="px-2 py-0.5" onClick={props.onClick} disabled={props.disabled}>
 			<Text.Body2>Refresh</Text.Body2>
 		</Button>
 	);
