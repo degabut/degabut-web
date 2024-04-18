1eacb5974f4be8861d6cd15c7acd3b987123c721
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-04-19 00:18:38 +0700

    clean up

--- src/libs/queue/providers/queue/hooks/player-position-updater.hook.ts
+++ src/libs/queue/providers/queue/hooks/player-position-updater.hook.ts
@@ -11,23 +11,9 @@ export const usePlayerPositionUpdater = ({ queue, setQueue }: Params) => {
 	let tickInterval: NodeJS.Timeout;
 
 	createEffect(() => {
-		if (!queue.isPaused && queue.nowPlaying) {
-			startTickInterval();
-		} else {
-			stopTickInterval();
-		}
+		clearInterval(tickInterval);
+		if (!queue.isPaused && queue.nowPlaying) tickInterval = setInterval(tick, 1000);
 	});
 
-	const startTickInterval = () => {
-		stopTickInterval();
-		if (!queue.isPaused && queue.nowPlaying) tickInterval = setInterval(tick, 1000);
-	};
-
-	const stopTickInterval = () => clearInterval(tickInterval);
-
-	const tick = () => {
-		if (!queue.nowPlaying) return;
-		const position = queue.position + 1000;
-		setQueue("position", Math.max(position, queue.nowPlaying.mediaSource.duration));
-	};
+	const tick = () => setQueue("position", (p) => p + 1000);
 };
