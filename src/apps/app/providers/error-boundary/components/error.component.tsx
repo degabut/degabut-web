db825f19ce74bc71caf2dafaf041c0bc1cb1f3c1
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-07 09:37:10 +0700

    add reload button

--- src/apps/app/providers/error-boundary/components/error.component.tsx
+++ src/apps/app/providers/error-boundary/components/error.component.tsx
@@ -3,6 +3,10 @@ import type { Component } from "solid-js";
 
 export const Error: Component<{ error: unknown }> = (props) => {
 	const copyToClipboard = () => navigator.clipboard.writeText(props.error as string);
+	const reload = (e: Event) => {
+		e.preventDefault();
+		location.reload();
+	};
 
 	// eslint-disable-next-line solid/reactivity
 	console.error(props.error);
@@ -14,9 +18,14 @@ export const Error: Component<{ error: unknown }> = (props) => {
 				<div class="flex flex-col text-xl pt-16 space-y-4">
 					<Text.H3>Something went wrong, check console for error details.</Text.H3>
 
-					<A class="underline underline-offset-2" href="/">
-						<Text.Body1>Go back</Text.Body1>
-					</A>
+					<div class="flex-row-center space-x-6">
+						<A class="underline underline-offset-2" href="/">
+							<Text.Body1>Go back</Text.Body1>
+						</A>
+						<A class="underline underline-offset-2" href="#" onClick={reload}>
+							<Text.Body1>Reload</Text.Body1>
+						</A>
+					</div>
 
 					<Text.Caption2 class="hover:underline underline-offset-2 cursor-pointer" onClick={copyToClipboard}>
 						Copy Error to Clipboard
