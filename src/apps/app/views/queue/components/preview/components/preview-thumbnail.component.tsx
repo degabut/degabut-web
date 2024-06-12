4121c4f091013ca6f217718a5a675f79fe2c0dfc
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-12 09:37:48 +0700

    change preview style

--- src/apps/app/views/queue/components/preview/components/preview-thumbnail.component.tsx
+++ src/apps/app/views/queue/components/preview/components/preview-thumbnail.component.tsx
@@ -46,7 +46,7 @@ const Thumbnail: Component<ThumbnailProps> = (props) => {
 			}}
 		>
 			<div class="absolute w-full h-full opacity-0 hover:opacity-100 transition flex items-end">
-				<div class="w-full flex flex-col justify-end min-h-[75%] bg-gradient-to-t from-black to-black/0">
+				<div class="w-full flex flex-col justify-end min-h-[50%] bg-gradient-to-t from-black via-black/75 to-black/0">
 					<div class="text-center space-y-2 truncate text-shadow px-4">
 						<Text.H1 truncate>{props.mediaSource.title}</Text.H1>
 						<Text.Body2 truncate>{props.mediaSource.creator}</Text.Body2>
@@ -54,7 +54,7 @@ const Thumbnail: Component<ThumbnailProps> = (props) => {
 					<QueueActions extraClass="w-full justify-evenly py-4" extraButtonClass="p-4" iconSize="lg" />
 				</div>
 			</div>
-			<img src={props.mediaSource.maxThumbnailUrl} class="h-full object-cover" />
+			<img src={props.mediaSource.maxThumbnailUrl} class="h-full object-cover rounded" />
 		</div>
 	);
 };
@@ -66,7 +66,7 @@ type SkeletonProps = {
 const Skeleton: Component<SkeletonProps> = (props) => {
 	return (
 		<div
-			class="max-w-[32rem] max-h-[32rem] mx-auto border border-neutral-850 flex items-center justify-center"
+			class="max-w-[32rem] max-h-[32rem] mx-auto border border-neutral-850 flex items-center justify-center rounded"
 			style={{
 				width: `${props.size}px`,
 				height: `${props.size}px`,
