4213ce182f4a52501a46d8cff1adeb6d1340a580
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-18 13:25:41 +0700

    update overlay

--- src/apps/desktop-overlay/views/queue/components/player-card.component.tsx
+++ src/apps/desktop-overlay/views/queue/components/player-card.component.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@common";
+import { Button, Divider, Text } from "@common";
 import { useDesktop } from "@desktop";
+import { SourceBadge, useLikeMediaSource } from "@media-source";
 import { QueueActions, QueueSeekSlider, VolumeSlider, useQueue } from "@queue";
 import { useSettings } from "@settings";
 import { Show, type Component } from "solid-js";
@@ -12,44 +13,77 @@ export const PlayerCard: Component = () => {
 
 	return (
 		<Card>
-			<div class="grow flex-col-center justify-end space-y-6 h-full">
+			<div class="relative flex-col-center justify-end space-y-8 h-full">
 				<Show when={queue.data.nowPlaying} keyed>
 					{({ mediaSource }) => (
-						<>
-							<img
-								src={mediaSource.maxThumbnailUrl || ""}
-								class="grow h-12 max-h-96 object-cover rounded"
-							/>
-
-							<div class="flex-col-center w-full text-shadow space-y-2 text-center">
-								<Text.H1 truncate class="w-full text-2xl text-shadow">
-									{mediaSource.title}
-								</Text.H1>
-								<Show when={mediaSource.creator} keyed>
-									{(c) => (
-										<Text.Body2 truncate class="w-full">
-											{c}
-										</Text.Body2>
-									)}
-								</Show>
-							</div>
-						</>
+						<img
+							src={mediaSource.maxThumbnailUrl || ""}
+							class="absolute top-0 left-0 opacity-25 w-full h-1/2 blur-[96px]"
+						/>
 					)}
 				</Show>
 
-				<QueueSeekSlider
-					disabled={queue.freezeState.seek}
-					max={queue.data.nowPlaying?.mediaSource.duration || 0}
-					onChange={(value) => queue.seek(value * 1000)}
-					value={(queue.data.position || 0) / 1000}
-				/>
+				<Show when={queue.data.nowPlaying} keyed>
+					{({ mediaSource }) => {
+						const like = useLikeMediaSource(() => mediaSource.id);
 
-				<QueueActions
-					extra={() => (
+						return (
+							<div class="relative w-full grow flex-row-center space-x-4">
+								<img
+									src={mediaSource.maxThumbnailUrl || ""}
+									class="grow h-full max-h-32 aspect-square object-cover rounded"
+								/>
+
+								<div class="flex flex-col w-full text-shadow truncate space-y-2.5">
+									<Text.H1 truncate class="w-full text-2xl text-shadow">
+										{mediaSource.title}
+									</Text.H1>
+									<Show when={mediaSource.creator} keyed>
+										{(c) => (
+											<div class="flex-row-center space-x-2.5">
+												<SourceBadge type={mediaSource.type} size="lg" />
+												<Text.Body2 truncate class="w-full">
+													{c}
+												</Text.Body2>
+											</div>
+										)}
+									</Show>
+
+									<div>
+										<Button
+											flat
+											theme={like?.isLiked() ? "brand" : "default"}
+											icon={like?.isLiked() ? "heart" : "heartLine"}
+											class="p-4"
+											iconSize="lg"
+											onClick={like?.toggle}
+										/>
+									</div>
+								</div>
+							</div>
+						);
+					}}
+				</Show>
+
+				<div class="w-full relative">
+					<QueueSeekSlider
+						disabled={queue.freezeState.seek}
+						max={queue.data.nowPlaying?.mediaSource.duration || 0}
+						onChange={(value) => queue.seek(value * 1000)}
+						value={(queue.data.position || 0) / 1000}
+					/>
+				</div>
+
+				<div class="w-full relative">
+					<QueueActions extraClass="justify-between w-full" extraButtonClass="p-4" iconSize="lg" />
+
+					<Divider dark extraClass="mt-8 mb-4" />
+
+					<div class="flex-row-center w-full">
 						<Show when={settings["discord.rpc"]}>
 							<VolumeSlider
-								extraButtonClass="p-3 md:px-6"
-								iconSize="lg"
+								extraButtonClass="p-2.5"
+								iconSize="md"
 								value={settings["botVolumes"][queue.bot().id]}
 								onChange={(value) => {
 									setSettings("botVolumes", { [queue.bot().id]: value });
@@ -63,11 +97,8 @@ export const PlayerCard: Component = () => {
 								}}
 							/>
 						</Show>
-					)}
-					extraClass="justify-evenly w-full"
-					extraButtonClass="p-3 md:px-6"
-					iconSize="lg"
-				/>
+					</div>
+				</div>
 			</div>
 		</Card>
 	);
