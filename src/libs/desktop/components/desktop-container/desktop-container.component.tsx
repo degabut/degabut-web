d50de84b99c684b3932bae9bdfe3969aa825e710
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-06-20 15:19:06 +0700

    calculate size based on zoom level

--- src/libs/desktop/components/desktop-container/desktop-container.component.tsx
+++ src/libs/desktop/components/desktop-container/desktop-container.component.tsx
@@ -1,13 +1,31 @@
+import { DelayUtil } from "@common";
 import { IS_DESKTOP } from "@constants";
-import { Show, type ParentComponent } from "solid-js";
+import { Show, createSignal, onMount, type ParentComponent } from "solid-js";
 
 export const DesktopContainer: ParentComponent = (props) => {
+	const [size, setSize] = createSignal("2rem");
+
+	const updateSize = DelayUtil.throttle(() => {
+		const basePadding = 2;
+		const zoomLevel = window.devicePixelRatio;
+		const adjustedPadding = basePadding / zoomLevel;
+		setSize(`${adjustedPadding}rem`);
+	}, 250);
+
+	onMount(() => {
+		if (!IS_DESKTOP) return;
+
+		updateSize();
+		window.addEventListener("resize", updateSize);
+		return () => window.removeEventListener("resize", updateSize);
+	});
+
 	return (
-		<div class="bg-neutral-850 h-full" classList={{ "pt-8": IS_DESKTOP }}>
+		<div class="bg-neutral-850 h-full" style={{ "padding-top": IS_DESKTOP ? size() : undefined }}>
 			<Show when={IS_DESKTOP}>
 				<div
-					class="absolute h-8 top-0 left-0 w-full z-50 bg-neutral-850"
-					style={{ "-webkit-app-region": "drag" }}
+					class="absolute top-0 left-0 w-full z-[1000] bg-neutral-850"
+					style={{ "-webkit-app-region": "drag", height: size() }}
 				/>
 			</Show>
 			{props.children}
