c2aec6c50313f895de90eb6e7af928f96b9a37c1
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-07-03 19:14:33 +0700

    use proper a component

--- src/apps/app/views/recommendation/components/recap-banner.component.tsx
+++ src/apps/app/views/recommendation/components/recap-banner.component.tsx
@@ -1,4 +1,4 @@
-import { Icon, Text } from "@common";
+import { A, Icon, Text } from "@common";
 import { RecapRoutes } from "@recap/routes";
 import type { Component } from "solid-js";
 
@@ -8,13 +8,13 @@ type RecapBannerProps = {
 
 export const RecapBanner: Component<RecapBannerProps> = (props) => {
 	return (
-		<a
+		<A
 			href={RecapRoutes.Recap}
 			target="_blank"
 			class="flex space-x-4 items-center border border-brand-800 text-brand-600 hover:bg-brand-500/5 rounded px-6 py-3 w-full"
 		>
 			<Icon name="stars" size="xl" />
 			<Text.H3>Check Out Your {props.year} Recap</Text.H3>
-		</a>
+		</A>
 	);
 };
