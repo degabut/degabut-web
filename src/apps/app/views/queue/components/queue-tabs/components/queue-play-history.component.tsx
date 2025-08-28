3d89391fb92226d31126067c74682fbd06522b07
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2025-08-28 22:36:47 +0800

    remove unnecessary show

--- src/apps/app/views/queue/components/queue-tabs/components/queue-play-history.component.tsx
+++ src/apps/app/views/queue/components/queue-tabs/components/queue-play-history.component.tsx
@@ -1,22 +1,18 @@
 import { MediaSources } from "@media-source";
 import { useQueue } from "@queue";
-import { Show, type Component } from "solid-js";
+import { type Component } from "solid-js";
 
 export const QueuePlayHistory: Component = () => {
 	const queue = useQueue()!;
 
 	return (
-		<Show when={queue.data.history} keyed>
-			{(tracks) => (
-				<MediaSources.List
-					data={tracks}
-					mediaSourceProps={(t) => ({
-						mediaSource: t.mediaSource,
-						requestedBy: t.requestedBy,
-						inQueue: queue.data.tracks?.some((qt) => qt.mediaSource.id === t.mediaSource.id),
-					})}
-				/>
-			)}
-		</Show>
+		<MediaSources.List
+			data={queue.data.history}
+			mediaSourceProps={(t) => ({
+				mediaSource: t.mediaSource,
+				requestedBy: t.requestedBy,
+				inQueue: queue.data.tracks?.some((qt) => qt.mediaSource.id === t.mediaSource.id),
+			})}
+		/>
 	);
 };
